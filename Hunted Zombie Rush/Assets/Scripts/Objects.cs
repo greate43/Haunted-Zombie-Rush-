c4eada3304fb9e24ac78d024b2c6848f263c94aa
SALMAN KHAN <greate43.sk@gmail.com>
2016-12-12 21:53:44 +0500

    moving path working

---
+++ Hunted Zombie Rush/Assets/Scripts/Objects.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objects : MonoBehaviour {
+
+    // Use this for initialization
+
+    [SerializeField] private float ObjectSpeed=4;
+    [SerializeField] private float ResetPosition=-21.65f;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.left*(ObjectSpeed*Time.deltaTime));
+        if (transform.localPosition.x<=ResetPosition)
+        {
+            Vector3 NewPos = new Vector3(59.6f, transform.position.y, transform.position.z);
+            transform.position = NewPos;
+        }
+	}
+}
---
+++ Hunted Zombie Rush/Assets/Scripts/Rock.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour {
+
+    [SerializeField] Vector3 TopPosition;
+    [SerializeField] Vector3 BottomPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
