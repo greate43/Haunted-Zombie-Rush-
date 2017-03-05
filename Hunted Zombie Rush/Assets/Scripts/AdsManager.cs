7d7fbc07cfff09f6236f4bb583c01c79c2d29011
SALMAN KHAN <greate43.sk@gmail.com>
2017-03-05 15:08:27 +0500

    simple ads added

--- Hunted Zombie Rush/Assets/Scripts/AdsManager.cs
+++ Hunted Zombie Rush/Assets/Scripts/AdsManager.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class AdsManager : MonoBehaviour
 {
     public static AdsManager Instance;
-    private RewardBasedVideoAd _extraLivesVideosRewards;
+//    private RewardBasedVideoAd _extraLivesVideosRewards;
     private InterstitialAd _interstitial;
+
+
 
     private void Awake()
     {
@@ -15,48 +17,40 @@ public class AdsManager : MonoBehaviour
 
     private void Start()
     {
-        // Reward based video instance is a singleton. Register handlers once to
-        // avoid duplicate events.
-
-        _extraLivesVideosRewards = RewardBasedVideoAd.Instance;
-        Debug.Log("start ");
-        // Ad event fired when the rewarded video ad
-        // has been received.
-        _extraLivesVideosRewards.OnAdLoaded += HandleRewardBasedVideoLoaded;
-        // has failed to load.
-        _extraLivesVideosRewards.OnAdFailedToLoad += HandleRewardBasedVideoFailedToLoad;
-        // is opened.
-        _extraLivesVideosRewards.OnAdOpening += HandleRewardBasedVideoOpened;
-        // has started playing.
-        _extraLivesVideosRewards.OnAdStarted += HandleRewardBasedVideoStarted;
-        // has rewarded the user.
-        _extraLivesVideosRewards.OnAdRewarded += HandleRewardBasedVideoRewarded;
-        // is closed.
-        _extraLivesVideosRewards.OnAdClosed += HandleRewardBasedVideoClosed;
-        // is leaving the application.
-        _extraLivesVideosRewards.OnAdLeavingApplication += HandleRewardBasedVideoLeftApplication;
- 
-        Debug.Log("ends ");
-    }
-
-    private void Update()
-    {
-//        if (Input.GetMouseButtonDown(0))
-//        {
-//            Debug.Log("clicked");
-//            ShowToast.Instance.ShowMyToastMethod("clicked");
-//            
-//        }
+//        // Reward based video instance is a singleton. Register handlers once to
+//        // avoid duplicate events.
+//
+//        _extraLivesVideosRewards = RewardBasedVideoAd.Instance;
+//
+//        // Ad event fired when the rewarded video ad
+//        // has been received.
+//        _extraLivesVideosRewards.OnAdLoaded += HandleRewardBasedVideoLoaded;
+//        // has failed to load.
+//        _extraLivesVideosRewards.OnAdFailedToLoad += HandleRewardBasedVideoFailedToLoad;
+//        // is opened.
+//        _extraLivesVideosRewards.OnAdOpening += HandleRewardBasedVideoOpened;
+//        // has started playing.
+//        _extraLivesVideosRewards.OnAdStarted += HandleRewardBasedVideoStarted;
+//        // has rewarded the user.
+//        _extraLivesVideosRewards.OnAdRewarded += HandleRewardBasedVideoRewarded;
+//        // is closed.
+//        _extraLivesVideosRewards.OnAdClosed += HandleRewardBasedVideoClosed;
+//        // is leaving the application.
+//        _extraLivesVideosRewards.OnAdLeavingApplication += HandleRewardBasedVideoLeftApplication;
+// 
+    
     }
 
 
-    private void RequestInterstitial()
+
+
+    public void RequestInterstitial()
     {
         // These ad units are configured to always serve test ads.
 #if UNITY_EDITOR
         var adUnitId = "unused";
 #elif UNITY_ANDROID
-        string adUnitId = "ca-app-pub-3940256099942544/1033173712";
+        string adUnitId = "ca-app-pub-9288334882409969/2876393337";
 #elif UNITY_IPHONE
         string adUnitId = "ca-app-pub-3940256099942544/4411468910";
 #else
@@ -77,20 +71,20 @@ public class AdsManager : MonoBehaviour
         _interstitial.LoadAd(CreateAdRequest());
     }
 
-    private void RequestRewardBasedVideo()
-    {
-#if UNITY_EDITOR
-        var adUnitId = "unused";
-#elif UNITY_ANDROID
-        string adUnitId = "ca-app-pub-6743313647495679/8141927146";
-#elif UNITY_IPHONE
-        string adUnitId = "INSERT_AD_UNIT_HERE";
-#else
-        string adUnitId = "unexpected_platform";
-#endif
-        ShowToast.Instance.ShowMyToastMethod("Requesting Rewards Based Videos Ads");
-       _extraLivesVideosRewards.LoadAd(CreateAdRequest(), adUnitId);
-    }
+//    public void RequestRewardBasedVideo()
+//    {
+//#if UNITY_EDITOR
+//        var adUnitId = "unused";
+//#elif UNITY_ANDROID
+//        string adUnitId = "ca-app-pub-6743313647495679/8141927146";
+//#elif UNITY_IPHONE
+//        string adUnitId = "INSERT_AD_UNIT_HERE";
+//#else
+//        string adUnitId = "unexpected_platform";
+//#endif
+//        ShowToast.Instance.ShowMyToastMethod("Requesting Rewards Based Videos Ads");
+//       _extraLivesVideosRewards.LoadAd(CreateAdRequest(), adUnitId);
+//    }
 
 
     private AdRequest CreateAdRequest()
@@ -101,19 +95,19 @@ public class AdsManager : MonoBehaviour
             .Build();
     }
 
-    public void LoadRewardVideoAds()
-    {
-        if (_extraLivesVideosRewards.IsLoaded())
-            _extraLivesVideosRewards.Show();
-        else
-        {
-            ShowToast.Instance.ShowMyToastMethod("Reward based video ad is not ready yet");
-         
-            RequestRewardBasedVideo();
-        }
-    }
+//    public void LoadRewardVideoAds()
+//    {
+//        if (_extraLivesVideosRewards.IsLoaded())
+//            _extraLivesVideosRewards.Show();
+//        else
+//        {
+//            ShowToast.Instance.ShowMyToastMethod("Reward based video ad is not ready yet");
+//         
+//            RequestRewardBasedVideo();
+//        }
+//    }
 
-    private void ShowInterstitial()
+    public void ShowInterstitial()
     {
         if (_interstitial.IsLoaded())
             _interstitial.Show();
@@ -123,75 +117,74 @@ public class AdsManager : MonoBehaviour
 
     #region Interstitial callback handlers
 
-    public void HandleInterstitialLoaded(object sender, EventArgs args)
+    private void HandleInterstitialLoaded(object sender, EventArgs args)
     {
         print("HandleInterstitialLoaded event received");
     }
 
-    public void HandleInterstitialFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    private void HandleInterstitialFailedToLoad(object sender, AdFailedToLoadEventArgs args)
     {
         print(
             "HandleInterstitialFailedToLoad event received with message: " + args.Message);
     }
 
-    public void HandleInterstitialOpened(object sender, EventArgs args)
+    private void HandleInterstitialOpened(object sender, EventArgs args)
     {
         print("HandleInterstitialOpened event received");
     }
 
-    public void HandleInterstitialClosed(object sender, EventArgs args)
+    private void HandleInterstitialClosed(object sender, EventArgs args)
     {
         print("HandleInterstitialClosed event received");
     }
 
-    public void HandleInterstitialLeftApplication(object sender, EventArgs args)
+    private void HandleInterstitialLeftApplication(object sender, EventArgs args)
     {
         print("HandleInterstitialLeftApplication event received");
     }
 
     #endregion
 
-    #region RewardBasedVideo callback handlers
-
-    public void HandleRewardBasedVideoLoaded(object sender, EventArgs args)
-    {
-        ShowToast.Instance.ShowMyToastMethod("HandleRewardBasedVideoLoaded event received");
-    }
-
-    public void HandleRewardBasedVideoFailedToLoad(object sender, AdFailedToLoadEventArgs args)
-    {
-        ShowToast.Instance.ShowMyToastMethod(
-            "HandleRewardBasedVideoFailedToLoad event received with message: " + args.Message);
-        RequestRewardBasedVideo();
-    }
-
-    public void HandleRewardBasedVideoOpened(object sender, EventArgs args)
-    {
-        ShowToast.Instance.ShowMyToastMethod("HandleRewardBasedVideoOpened event received");
-    }
-
-    public void HandleRewardBasedVideoStarted(object sender, EventArgs args)
-    {
-        ShowToast.Instance.ShowMyToastMethod("HandleRewardBasedVideoStarted event received");
-    }
-
-    public void HandleRewardBasedVideoClosed(object sender, EventArgs args)
-    {
-        ShowToast.Instance.ShowMyToastMethod("HandleRewardBasedVideoClosed event received");
-    }
-
-    public void HandleRewardBasedVideoRewarded(object sender, Reward args)
-    {
-        var type = args.Type;
-        var amount = args.Amount;
-        ShowToast.Instance.ShowMyToastMethod(
-            "Reward is " + amount + " " + type);
-    }
-
-    public void HandleRewardBasedVideoLeftApplication(object sender, EventArgs args)
-    {
-        ShowToast.Instance.ShowMyToastMethod("HandleRewardBasedVideoLeftApplication event received");
-    }
-
-    #endregion
+//    #region RewardBasedVideo callback handlers
+//
+//    public void HandleRewardBasedVideoLoaded(object sender, EventArgs args)
+//    {
+//        ShowToast.Instance.ShowMyToastMethod("HandleRewardBasedVideoLoaded event received");
+//    }
+//
+//    public void HandleRewardBasedVideoFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+//    {
+//        ShowToast.Instance.ShowMyToastMethod(args.Message);
+//      
+//    }
+//
+//    public void HandleRewardBasedVideoOpened(object sender, EventArgs args)
+//    {
+//        ShowToast.Instance.ShowMyToastMethod("HandleRewardBasedVideoOpened event received");
+//    }
+//
+//    public void HandleRewardBasedVideoStarted(object sender, EventArgs args)
+//    {
+//        ShowToast.Instance.ShowMyToastMethod("HandleRewardBasedVideoStarted event received");
+//    }
+//
+//    public void HandleRewardBasedVideoClosed(object sender, EventArgs args)
+//    {
+//        ShowToast.Instance.ShowMyToastMethod("HandleRewardBasedVideoClosed event received");
+//    }
+//
+//    public void HandleRewardBasedVideoRewarded(object sender, Reward args)
+//    {
+//        var type = args.Type;
+//        var amount = args.Amount;
+//        ShowToast.Instance.ShowMyToastMethod(
+//            "Reward is " + amount + " " + type);
+//    }
+//
+//    public void HandleRewardBasedVideoLeftApplication(object sender, EventArgs args)
+//    {
+//        ShowToast.Instance.ShowMyToastMethod("HandleRewardBasedVideoLeftApplication event received");
+//    }
+//
+//    #endregion
 }
--- Hunted Zombie Rush/Assets/Scripts/GameOverManager.cs
+++ Hunted Zombie Rush/Assets/Scripts/GameOverManager.cs
@@ -4,6 +4,11 @@ public class GameOverManager : MonoBehaviour
 {
     private Animator _anim;
 
+    [SerializeField] private float _probablityOfAdsBeingShowed = 2f;
+//    [SerializeField] private GameObject _rewardsAdsDialog;
+    private float _randomNumber;
+
+
     public void Restart()
     {
         GameManager.Instance.PlayAgain();
@@ -14,14 +19,59 @@ public class GameOverManager : MonoBehaviour
         GameManager.Instance.BackToMainMenu();
     }
 
+//    private void Awake()
+//    {
+//        Assert.IsNotNull(_rewardsAdsDialog);
+//    }
+
     private void Start()
     {
+        
         _anim = GetComponent<Animator>();
+        _randomNumber = Random.Range(0, 5);
+         AdsManager.Instance.RequestInterstitial();
+
     }
 
     private void Update()
     {
+
+//        if (_randomNumber < _probablityOfAdsBeingShowed && GameManager.Instance.GameOver && _allow)
+//        {
+//            _rewardsAdsDialog.SetActive(true);
+//            TriggeredGameOverAnimation(false);
+//        }
+      TriggeredGameOverAnimation();
+    }
+
+//    public void DontShowRewardAds()
+//    {
+//        TriggeredGameOverAnimation(true);
+//    }
+
+//    public void ShowRewardAds()
+//    {
+//        AdsManager.Instance.LoadRewardVideoAds();
+//        _rewardsAdsDialog.SetActive(false);
+//    }
+
+    private void TriggeredGameOverAnimation()
+    {
+       
+        
         if (GameManager.Instance.GameOver)
+        {
+//         _rewardsAdsDialog.SetActive(false);
+
             _anim.SetTrigger("GameOver");
+            
+           
+            if (_randomNumber<_probablityOfAdsBeingShowed)
+            {
+                
+                AdsManager.Instance.ShowInterstitial();
+            }
+
+        }
     }
 }
